3fe051ac11fcec0c285467f6090bf78111b42e97
Simon <fujizhang2525@gmail.com>
2024-02-22 01:03:56 -0500

    aniamtion

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-// Specify the file extension you want to import
 declare module "*.yml" {
   const value: any; // Add type definitions here if desired
   export default value;
